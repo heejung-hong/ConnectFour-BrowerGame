9197732dbe9a5f99f02ceb2fda0bf3e627132b09
heejung1180 <heejung1180@outlook.com>
2023-09-07 11:45:59 -0700

    wireframe and starting plan for game

---
+++ README.md
@@ -0,0 +1,8 @@
+CONNECT FOUR
+
+1. ScreenShot
+
+
+2. Wireframe
+
+![Alt text](<Screenshot 2023-09-07 at 9.43.36 AM.png>)
---
+++ main.js
@@ -0,0 +1,11 @@
+// State Variables:
+  // turn: 1 and -1 to represent players because the visualization can change, also same value as turn
+  // board: 2D array
+    // null or 0 to represent empty space, no player
+    // 1 or -1 player at the cell
+  // winner: 
+    // null for no winner
+    // 1 or -1 for winner
+    // 'T' for tie
+  // render function to play game
+
